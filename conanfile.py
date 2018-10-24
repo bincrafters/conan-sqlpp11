c34ac8a7ebfe9b6c6551ee7ba799f20459a6fb47
Croydon <cr0ydon@outlook.com>
2018-10-25 01:40:24 +0200

    Apply latest templates

--- conanfile.py
+++ conanfile.py
@@ -12,25 +12,25 @@ class sqlpp11Conan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     url = "https://github.com/bincrafters/conan-sqlpp11"
     homepage = "https://github.com/rbock/sqlpp11"
+    author = "Bincrafters <bincrafters@gmail.com>"
     license = "BSD 2-Clause"
     exports = ["LICENSE.md"]
     exports_sources = ["CMakeLists.txt"]
     generators = "cmake"
-    source_subfolder = "source_subfolder"
-    build_subfolder = "build_subfolder"
-    requires = 'date/2.4@bincrafters/stable'
+    _source_subfolder = "source_subfolder"
+    _build_subfolder = "build_subfolder"
+    requires = "date/2.4@bincrafters/stable"
 
     def source(self):
-        source_url = "https://github.com/rbock/sqlpp11"
-        tools.get("{0}/archive/{1}.tar.gz".format(source_url, self.version))
+        tools.get("{0}/archive/{1}.tar.gz".format(self.homepage, self.version), sha256="107e2e8ae6c37ba8db2aa1c290708ac6b651f9597c5619af6e2b84bbc5ab74e1")
         extracted_dir = self.name + "-" + self.version
-        os.rename(extracted_dir, self.source_subfolder)
+        os.rename(extracted_dir, self._source_subfolder)
 
     def configure_cmake(self):
         cmake = CMake(self)
         cmake.definitions["ENABLE_TESTS"] = False
         cmake.definitions['HinnantDate_ROOT_DIR'] = self.deps_cpp_info['date'].include_paths[0]
-        cmake.configure(build_folder=self.build_subfolder)
+        cmake.configure(build_folder=self._build_subfolder)
         return cmake
 
     def build(self):
@@ -38,7 +38,7 @@ class sqlpp11Conan(ConanFile):
         cmake.build()
 
     def package(self):
-        self.copy(pattern="LICENSE", dst="licenses", src=self.source_subfolder)
+        self.copy(pattern="LICENSE", dst="licenses", src=self._source_subfolder)
         cmake = self.configure_cmake()
         cmake.install()
 
